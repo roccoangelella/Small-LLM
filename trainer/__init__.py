95b64614c9eab4e0d6ef4bdc29e5aef110b61dd0
Rocco Angelella <rocchissimo936@gmail.com>
2026-08-03 13:05:15 +0200

    feat(trainer): export hybrid optimizer interfaces

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -8,13 +8,20 @@ from .engine import (
     TrainerEngine,
     TrainingSession,
     build_adamw,
+    build_optimizer,
     generate_token_ids,
     seed_everything,
 )
 from .identity import canonical_hash, checkpoint_identity, saved_checkpoint_identity
+from .optimizer import (
+    HybridMuonAdamW,
+    build_hybrid_muon_adamw,
+    optimizer_parameter_routing,
+)
 from .schedule import TokenLRScheduler
 
 __all__ = [
+    "HybridMuonAdamW",
     "LiveBlockConsumer",
     "PreparedBlockDecoder",
     "SchemaV2ShardReader",
@@ -25,9 +32,12 @@ __all__ = [
     "TrainerEngine",
     "TrainingSession",
     "build_adamw",
+    "build_hybrid_muon_adamw",
+    "build_optimizer",
     "canonical_hash",
     "checkpoint_identity",
     "generate_token_ids",
+    "optimizer_parameter_routing",
     "saved_checkpoint_identity",
     "seed_everything",
 ]
