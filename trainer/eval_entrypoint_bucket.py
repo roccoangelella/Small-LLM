3d19784cab62297b71f4a2e3e1a7552874ecf9f0
Rocco Angelella <rocchissimo936@gmail.com>
2026-09-02 10:54:40 +0200

    Align bucket eval entrypoint with prompt-suite bucket transport

--- trainer/eval_entrypoint_bucket.py
+++ trainer/eval_entrypoint_bucket.py
@@ -3,12 +3,12 @@
 from __future__ import annotations
 
 from trainer import eval_entrypoint, eval_suite, post_pretraining_prompt_suite
-from trainer.bucket_checkpoint_eval import download_verified_checkpoint
+from trainer.post_pretraining_prompt_suite_bucket import download_verified_bucket_checkpoint
 
 
 def main() -> int:
-    post_pretraining_prompt_suite.download_verified_checkpoint = download_verified_checkpoint
-    eval_suite.download_verified_checkpoint = download_verified_checkpoint
+    post_pretraining_prompt_suite.download_verified_checkpoint = download_verified_bucket_checkpoint
+    eval_suite.download_verified_checkpoint = download_verified_bucket_checkpoint
     return eval_entrypoint.main()
 
 
